d26c2175087625a7348c1458616139b78bf8eb3a
DaniKay <dankoretsky@gmail.com>
2020-12-07 23:27:42 +0200

    generic UPPER TEXT search

--- SHMCD-fixsave.py
+++ SHMCD-fixsave.py
@@ -8,7 +8,7 @@ def write(name, data):
     with open(name, "wb") as fid:
         fid.write(data)
 
-BLOB_END = 'JTANKJTANKJTANKJTANH@TAN'
+WINDOW = 128
 DELIMITER = '\x05'
 
 BACKUP_NAME = 'sh.mcd.specialbak'
@@ -23,7 +23,28 @@ def main():
     print 'backing up sh.mcd into {}'.format(BACKUP_NAME)
     write(BACKUP_NAME, d)
     
-    lengthFix = d.find(DELIMITER, d.rfind(BLOB_END) + len(BLOB_END)) + 3
+    #method No. 1
+    i = len(d)-WINDOW
+    while i > 0:
+        upperBlob = d[i:i+WINDOW]
+        if (upperBlob.isalpha() and upperBlob.isupper()):
+            break
+        i -= 1
+
+    if i > 0:
+        print 'found blob upper text {}'.format(d[i:i+WINDOW])
+        i += WINDOW
+
+    #method 2
+    elif i == 0:
+        i = d.rfind('JTAN')
+
+    else:
+        print 'wtf? i is negative? i: {} len(d): {}'.format(i, len(d))
+
+    endOfUPPERTEXT = i
+
+    lengthFix = d.find(DELIMITER, endOfUPPERTEXT)
     start = d.find('ZB', lengthFix)
     end = d.find(DELIMITER, start)
     dn = d[:start] + COUNTER_FINISHED + d[end:]
@@ -31,10 +52,13 @@ def main():
     print 'string before: {}'.format(repr(d[start-1:end+1]))
     print 'string after:  {}'.format(repr(dn[start-1:end+1 + len(dn) - len(d)]))
 
+    print 'positions: start {} end {} lengthFix {} endOfUPPERTEXT {}'.format(start, end, lengthFix, endOfUPPERTEXT)
+
     b = bytearray(dn)
     print 'fixing length: ', len(dn) - len(d)
     b[lengthFix] += len(dn) - len(d)
     write('sh.mcd', b)
+    os.system('pause')
 
 if __name__ == '__main__':
     main()
